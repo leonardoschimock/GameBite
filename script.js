2b4ab0c4449967b63eb978b1144b9a0a38433c39
Leonardo Schimock <dev.leonardosch@hotmail.com>
2026-04-07 00:36:56 -0300

    Responsividade e ajustes finos

--- script.js
+++ script.js
@@ -74,3 +74,31 @@ if (campoBusca) {
     }
 
 });
+
+// ===== HIGHLIGHT DO FOOTER =====
+const links = document.querySelectorAll("nav a");
+
+links.forEach(link => {
+    link.addEventListener("click", function () {
+
+        const href = this.getAttribute("href");
+
+        if (href.startsWith("#")) {
+
+            const idDestino = href.substring(1);
+            const elemento = document.getElementById(idDestino);
+
+            if (elemento) {
+
+                setTimeout(() => {
+                    elemento.classList.add("highlight");
+
+                    setTimeout(() => {
+                        elemento.classList.remove("highlight");
+                    }, 2000);
+
+                }, 600);
+            }
+        }
+    });
+});
